b29ada8b991076134b2c406af11f833516df4a04
JiggyDev <jeradybrew18@gmail.com>
2025-04-27 00:44:49 +0300

    darkmode toggle added

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 
 const settingsMenu = document.querySelector(`.profile-settings`);
 
+const darkBtn= document.getElementById(`dark-btn`)
 
 
 
 function settingsMenuToggle(){
     settingsMenu.classList.toggle(`profile-settings-height`);
 }
+
+darkBtn.onclick = function(){
+    darkBtn.classList.toggle(`dark-btn-on`);
+}
+
+
